f35622a160d7a7b8854eee86405297381ee07fe8
Anna <annegg03@byu.edu>
2025-01-27 22:38:39 -0700

    Queen file unnecessary but idk how to delete

---
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -0,0 +1,34 @@
+//package chess;
+//
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.List;
+//
+//public class QueenMovesCalculator implements PieceMovesCalculator {
+//    private ChessPiece piece;
+//    private ChessPosition newPosition;
+//    private ChessMove move;
+//    private ChessPiece myPiece;
+//
+//    @Override
+//    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+//        List<ChessMove> moves = new ArrayList<>();
+//        myPiece = board.getPiece(myPosition);
+//        if (myPosition.getColumn()-1 >= 1 && myPosition.getRow()-1 >= 1 && myPosition.getColumn()+1 <= 8 && myPosition.getRow()+1 <= 8) {
+//            for (int i = -1; i <= 1; i++) {
+//                for (int j = -1; j <= 1; j++) {
+//                    if (i==0 && j==0) {
+//                        continue;
+//                    }
+//                    newPosition = new ChessPosition(myPosition.getColumn()+i,myPosition.getRow()+j);
+//                    piece = board.getPiece(newPosition);
+//                    if (piece == null) {
+//                        move = new ChessMove(myPosition,newPosition,myPiece.getPieceType());
+//                        moves.add(move);
+//                    }
+//                }
+//            }
+//        }
+//        return moves;
+//    }
+//}
